18a1f20d956454a5b3deda54f332ddc88d4abfa7
Rahul Kumar <rahulloni000@gmail.com>
2022-09-14 08:58:22 +0545

    Bottom Navigation

--- app/src/main/java/com/example/walmart/Home.java
+++ app/src/main/java/com/example/walmart/Home.java
@@ -1,14 +1,37 @@
 package com.example.walmart;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class Home extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
+public class Home extends AppCompatActivity implements
+        BottomNavigationView.OnNavigationItemSelectedListener{
+    BottomNavigationItemView bottomNavigationItemView;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+
+        switch (item.getItemId()){
+            case R.id.items2:
+                Intent intent=new Intent(Home.this,Login.class);
+                startActivity(intent);
+                break;
+        }
+        return true;
     }
 }
